a3f945e6b5dd77379e4265e20e192f06b14e530a
Rizky Alfito <ikycollege@gmail.com>
2023-11-22 22:47:21 +0700

    add landing-page

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -3,9 +3,11 @@ import Check from '../views/pages/check';
 import Tips from '../views/pages/tips';
 import Forum from '../views/pages/forum';
 import ContactUs from '../views/pages/contact-us';
+import LandingPage from '../views/pages/landing-page';
 
 const routes = {
   '/': Home, // default page
+  '/landing-page': LandingPage,
   '/home': Home,
   '/check': Check,
   '/tips': Tips,
