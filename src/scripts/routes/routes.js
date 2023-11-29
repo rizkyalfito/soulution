fb905b6380c942cdb1e821e29db833c56eedd9b7
mirza <mirzamaulana525@gmail.com>
2023-11-29 09:23:55 +0700

    Membuat halaman Login dan SignUp

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -4,15 +4,19 @@ import Tips from '../views/pages/tips';
 import Forum from '../views/pages/forum';
 import ContactUs from '../views/pages/contact-us';
 import LandingPage from '../views/pages/landing-page';
+import Login from '../views/pages/login';
+import SignUp from '../views/pages/signup';
 
 const routes = {
-  '/': LandingPage, // default page
+  '/': Login, // default page
   '/landing-page': LandingPage,
   '/home': Home,
   '/check': Check,
   '/tips': Tips,
   '/forum': Forum,
   '/contact-us': ContactUs,
+  '/login': Login,
+  '/signup': SignUp,
 };
 
 export default routes;
