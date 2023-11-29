d72c3d9a007c18436c920aa4de278c3d95c5436e
irsyadielnur <irsyadihanif08@gmail.com>
2023-11-29 10:01:28 +0700

    tips hampir selesai tinggal pembuatan halaman detail

--- src/data/source-tips.js
+++ src/data/source-tips.js
@@ -13,6 +13,21 @@ class tipsSource {
       console.error('Error while fetching data:', error);
     }
   }
+
+  // eslint-disable-next-line consistent-return
+  static async LongTips() {
+    try {
+      const response = await fetch('./data/longTips-data.json');
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const responseJson = await response.json();
+      return responseJson.tipsJangkaPanjang;
+    } catch (error) {
+      console.error('Error while fetching data:', error);
+    }
+  }
 }
 
 export default tipsSource;
