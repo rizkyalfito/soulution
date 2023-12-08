2359ae21bc6d223b8f4b7440aace9558ef12997f
Rizky Alfito <ikycollege@gmail.com>
2023-12-08 22:08:03 +0700

    memindahkan article kedalam halaman tips, routing lagi

--- src/scripts/views/pages/tips.js
+++ src/scripts/views/pages/tips.js
@@ -1,5 +1,7 @@
 import { tipsSource } from '../../../data/source-tips';
 import { tipsShortTemplate, tipsLongTemplate } from '../templates/tipsTemplate';
+import { articleItemTemplate } from '../templates/articleCardTemplate';
+import articleSource from '../../../data/source-article';
 
 const Tips = {
   async render() {
@@ -24,14 +26,24 @@ const Tips = {
         <div class="d-flex align-items-center justify-content-end w-100 px-5 load-more">
           <button id="loadLongTips" type="submit">Load More</button>
         </div>
+
+        <h2 class="fs-3 px-5 pt-5 fw-bold">Temukan Artikel Berkaitan Dengan Kesehatan Mental</h2>
+
+        <div id="ArticleList"></div>
+        <div class="d-flex align-items-center justify-content-end w-100 px-5 load-more">
+          <button id="loadArticle" type="submit">Load More</button>
+        </div>
+
         `;
   },
 
   async afterRender() {
     const shortTipsContainer = document.querySelector('#shortTipsList');
     const longTipsContainer = document.querySelector('#longTipsList');
+    const articleContainer = document.querySelector('#ArticleList');
     const loadTipsShortBtn = document.querySelector('#loadShortTips');
     const loadTipsLongBtn = document.querySelector('#loadLongTips');
+    const loadArticleBtn = document.querySelector('#loadArticle')
 
     const loadMoreShortTips = async () => {
       const tips = await tipsSource.ShortTips();
@@ -60,10 +72,26 @@ const Tips = {
         loadTipsLongBtn.style.display = 'none';
       }
     };
+
+    const loadMoreArticle = async () =>{
+      const articles = await articleSource.article();
+      const start = articleContainer.children.length;
+      const end = start + 2;
+
+      articles.slice(start, end).forEach((article) => {
+        articleContainer.innerHTML += articleItemTemplate(article);
+      });
+      
+      if(end >= articles.length) {
+        loadArticleBtn.style.display = 'none';
+      }
+    };
     await loadMoreShortTips();
     await loadMoreLongTips();
+    await loadMoreArticle();
     loadTipsShortBtn.addEventListener('click', () => loadMoreShortTips());
     loadTipsLongBtn.addEventListener('click', () => loadMoreLongTips());
+    loadArticleBtn.addEventListener('click', () => loadMoreArticle())
   },
 };
 
