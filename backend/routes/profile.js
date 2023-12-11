d664adacfcfa65ca0cc83e5af1a31d49b0d77def
Rizky Alfito <ikycollege@gmail.com>
2023-12-11 23:15:39 +0700

    menambahkan fungsi logout dan get username dan email dari database untuk halaman profile

---
+++ backend/routes/profile.js
@@ -0,0 +1,33 @@
+// backend/routes/profile.js
+const express = require('express');
+const db = require('../data');
+
+const router = express.Router();
+
+router.post('/update-username', async (req, res) => {
+    const { username } = req.body;
+
+    if (username) {
+        try {
+            // Pastikan pengguna sudah login
+            if (req.session.user) {
+                // Simpan pembaruan username ke database
+                await db.updateUsername(req.session.user.email, username);
+
+                // Perbarui session dengan username yang baru
+                req.session.user.username = username;
+
+                res.json({ message: 'Username updated' });
+            } else {
+                res.status(401).json({ message: 'Unauthorized' });
+            }
+        } catch (error) {
+            console.error('Error during username update', error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
+    } else {
+        res.status(400).json({ message: 'Username is required' });
+    }
+});
+
+module.exports = router;
