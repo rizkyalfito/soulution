66f606543769a2e83520aaadae318d4ef8df5dc5
irsyadielnur <irsyadihanif08@gmail.com>
2023-12-11 21:07:53 +0700

    mulai fungsi forum dikit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');  // Tambahkan modul CORS
+const cors = require('cors'); // Tambahkan modul CORS
+const socket = require('socket.io');
 
 const app = express();
 const PORT = 3000;
 
+// eslint-disable-next-line no-undef
+const io = socket(app);
+
+// eslint-disable-next-line no-shadow
+io.on('connection', (socket) => {
+  console.log('membuat koneksi socket', socket.id);
+
+  socket.on('chat', () => {
+    // eslint-disable-next-line no-undef
+    io.sockets.emit('chat', data);
+  });
+});
 // Middleware for parsing JSON
 app.use(express.json());
 
 // Middleware for CORS
-app.use(cors());  // Tambahkan middleware CORS
+app.use(cors()); // Tambahkan middleware CORS
 
 // Serve static files
 app.use(express.static(path.join(__dirname, '../public')));
@@ -21,5 +34,5 @@ app.use('/api', routes);
 
 // Server listening
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
--- src/scripts/views/pages/forum.js
+++ src/scripts/views/pages/forum.js
@@ -1,15 +1,47 @@
-import forumTemplate from '../templates/forumTemplate';
-
 const Forum = {
   async render() {
     return `
-    <div id="forum"></div>
+    <div class="forum-container">
+        <h2 class="py-2 rounded-1 text-light text-center">Lets Talk With People</h2>
+        <p class="w-50 text-center">This is a discussion forum. You can use this forum to share your stories with others without worrying about your data being known to others.</p>
+
+        <div id="forumchat" class="forumChat-container">
+            <div id="chatbox" class="chatbox rounded-2">
+                
+            </div>
+
+            <div class="input-chat">
+                <form id="chatForm" action="">
+                    <input id="message" name="message" class="message" type="text" placeholder="Type a message here...">
+                    <button id="sendMessage" type="submit">kirim</button>
+                    <input class="image" type="image"  src="./images/icons/forumImgInput.svg" value=""/>
+                </form>
+            </div>
+        </div>
+    </div>
       `;
   },
 
   async afterRender() {
-    const forumContainer = document.querySelector('#forum');
-    forumContainer.innerHTML = forumTemplate();
+    // eslint-disable-next-line no-undef
+    const socket = io.connect('http://localhost:3000/api');
+
+    const sendMessage = document.getElementById('sendMessage');
+    const message = document.getElementById('message');
+    const chatbox = document.getElementById('chatbox');
+
+    sendMessage.addEventListener('click', (e) => {
+      e.preventDefault();
+      socket.emit('chat', {
+        message: message.value,
+      });
+    });
+
+    socket.io('chat', (data) => {
+      chatbox.innerHTML += `
+            <p><strong>${data.message}</strong></p>
+        `;
+    });
   },
 };
 
