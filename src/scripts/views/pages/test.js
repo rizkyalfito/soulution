4de916f26d8de43f367a0b6915051e900c589b2e
Rizky Alfito <ikycollege@gmail.com>
2023-12-16 22:42:26 +0700

    logic dari check

--- src/scripts/views/pages/test.js
+++ src/scripts/views/pages/test.js
@@ -1,57 +1,63 @@
 import questionsource from "../../../data/source-question";
 import questionTemplate from "../templates/questionTemplate";
+import calculatePSSScore from "../../utils/calculatePSSScore";
 
 const Test = {
-    async render() {
-      return `
-      
-  <div class=" w-100 rounded up-content">
-      <p><b>Complete the following questions according to your situation<br /> <br />
-      In the LAST MONTH, how often have you: </b></p>
-  </div>
-<div class="test-content">
+  async render() {
+    return `
+      <div class="w-100 rounded up-content">
+        <p><b>Complete the following questions according to your situation<br /> <br />
+        In the LAST MONTH, how often have you: </b></p>
+      </div>
+      <div class="test-content">
+        <div id="questionlist"></div>
+        <div class="next">
+          <div class="next-button">
+            <button id="next-btn" type="submit">Next</button>
+          </div>
+        </div>
+      </div>
+    `;
+  },
 
-  <div id="questionlist"></div>
-
-    <div class="next">
-      <div class="next-button">
-        <button id="next-btn" type="submit" >Next</button>
-       </div>
-    </div>
-  </div>
-`;
-    },
-  
-    async afterRender() {
-      const questions = await questionsource.Question();
+  async afterRender() {
+    const questions = await questionsource.Question();
     const questionContainer = document.querySelector('#questionlist');
-
+    const userAnswers = [];
     let currentQuestion = 0;
 
-    
-    document.getElementById('next-btn').addEventListener('click', () => {
-      
-      currentQuestion++;
-
+    const updateQuestion = () => {
       if (currentQuestion >= questions.length) {
-       
         document.querySelector('#next-btn').textContent = 'Finish';
-        document.querySelector('#next-btn').addEventListener('click', () => {
-         
-          window.location.href = '#/hasil';
-        });
+document.querySelector('#next-btn').addEventListener('click', () => {
+  const pssScore = calculatePSSScore(userAnswers);
+
+  // Simpan nilai pssScore ke sessionStorage
+  sessionStorage.setItem('pssScore', pssScore);
+
+  // Pindah ke halaman hasil
+  window.location.href = '#/hasil';
+});
       } else {
-        
         const nextQuestion = questionTemplate(questions[currentQuestion]);
         questionContainer.innerHTML = nextQuestion;
       }
+    };
+
+    document.getElementById('next-btn').addEventListener('click', () => {
+      const selectedAnswer = document.querySelector('input[name="value-radio"]:checked');
+
+      if (selectedAnswer) {
+        const answerValue = parseInt(selectedAnswer.value, 10);
+        userAnswers.push(answerValue);
+      }
+
+      currentQuestion++;
+      updateQuestion();
     });
 
-    
-    questionContainer.innerHTML += questionTemplate(questions[currentQuestion]);
+    updateQuestion();
   },
-  };
-  
-  
-  export default Test;
-  
+};
+
+export default Test;
--- src/scripts/views/templates/questionTemplate.js
+++ src/scripts/views/templates/questionTemplate.js
@@ -7,32 +7,18 @@ const questionTemplate = (question) => `
     <h3 class="test-soal">${question.text}</h3>
 
     <div class="radio-input">
-    <input value="value-1" name="value-radio" id="value-1" type="radio">
-    <label for="value-1">Never</label>
-    <input value="value-2" name="value-radio" id="value-2" type="radio">
-    <label for="value-2">Almost Never</label>
-    <input value="value-3" name="value-radio" id="value-3" type="radio">
-    <label for="value-3">Sometimes</label>
-    <input value="value-4" name="value-radio" id="value-4" type="radio">
-    <label for="value-4">Fairly Often</label>
-    <input value="value-5" name="value-radio" id="value-5" type="radio">
-    <label for="value-5">Very Often</label>
-  </div>
+      <input value="value-1" name="value-radio" id="value-1" type="radio">
+      <label for="value-1">Never</label>
+      <input value="value-2" name="value-radio" id="value-2" type="radio">
+      <label for="value-2">Almost Never</label>
+      <input value="value-3" name="value-radio" id="value-3" type="radio">
+      <label for="value-3">Sometimes</label>
+      <input value="value-4" name="value-radio" id="value-4" type="radio">
+      <label for="value-4">Fairly Often</label>
+      <input value="value-5" name="value-radio" id="value-5" type="radio">
+      <label for="value-5">Very Often</label>
+    </div>
   </div>
 `;
-const buttons = document.querySelectorAll('.buttonAnswer button');
-let total = 0;
-
-buttons.forEach(button => {
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        const answerValue = parseInt(this.dataset.value,10);
-        total += answerValue;
-
-        document.getElementById('totalValue').value = total;
-    });
-}); 
 
 export default questionTemplate;
-
